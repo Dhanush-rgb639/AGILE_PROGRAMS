f212a7c1ef221cd48173f3c0a1ed0909d4294cca
Dhanush <dhanushreddy73@gmail.com>
2026-08-11 10:37:15 +0530

    calculator.PY

---
+++ bad_calculator.py
@@ -0,0 +1,32 @@
+#bad_calculator.py
+class Calculator:
+
+    def calculate(self, a, b, operation):
+
+        if operation == "add":
+            return a + b
+
+        elif operation == "subtract":
+            return a - b
+
+        elif operation == "multiply":
+            return a * b
+
+        elif operation == "divide":
+            if b == 0:
+                raise ValueError("Division by Zero!")
+            return a / b
+
+        else:
+            raise ValueError("Invalid Operation")
+
+
+# Example Usage
+if __name__ == "__main__":
+
+    calc = Calculator()
+
+    print("10 + 5 =", calc.calculate(10, 5, "add"))
+    print("10 - 5 =", calc.calculate(10, 5, "subtract"))
+    print("10 * 5 =", calc.calculate(10, 5, "multiply"))
+    print("10 / 5 =", calc.calculate(10, 5, "divide"))
